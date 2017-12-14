a2c90d0911054c72bedeeec98bea848461a34293
Gil Gald <=gil.2006@gmail.com>
2017-12-14 01:18:26 -0800

    g

--- main.ps1
+++ main.ps1
@@ -1,1 +1,15 @@
-New-Item -name outputFile.txt -ItemType File
+$script1=(get-item env:qsscript1_url).Value
+$script2=(get-item env:qsscript2_url).Value
+$script3=(get-item env:qsscript3_url).Value
+
+
+Invoke-WebRequest -Uri $script1 -OutFile "c:\\downloadme1.txt"
+Invoke-WebRequest -Uri $script2 -OutFile "c:\\downloadme2.txt"
+Invoke-WebRequest -Uri $script2 -OutFile "c:\\downloadme3.txt"
+
+#$url ="https://raw.githubusercontent.com/QualiSystems/EyScriptsDemo/master/downloadme1.txt"
+#$output = "$PSScriptRoot\downloadme1.txt"
+#$output2 = "c:\\downloadme1.txt"
+#Invoke-WebRequest -Uri $url -OutFile $output2
+
+
