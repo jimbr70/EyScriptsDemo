06778aa80b22086ebe9188ee4229b5dc353a72cf
Gil Galditsky <gil.g@qualisystems.com>
2017-12-17 15:25:33 +0200

    d

--- main.ps1
+++ main.ps1
@@ -1,20 +1,27 @@
 $script_count=(get-item env:qsscript_count).Value
 $script_name = ""
 $output=""
+$logspath="c:\qs_scripts_logs.txt"
+
 
 #$pname=(get-item env:qspname).Value
 #$uname=(get-item env:qsuname).Value
 #$execute_command=(get-item env:qsexecute_command).Value
 
-
+New-Item $logspath -type file -force
+Add-Content $logspath "`nstarts script download"
 
 For ($i=1; $i -le $script_count; $i++) {
     $script_name ="qsscript{0}_url" -f $i
-
     Write-host  ( $script_name)
     $script_url=(get-item env:$script_name).Value
     Write-host  ( $script_url)
     $output="c:\\{0}.txt" -f $script_name 
+
+    $msg= "`n downloading url : {0}"-f $script_url 
+
+
+    Add-Content $logspath $msg
     Invoke-WebRequest -Uri $script_url -OutFile $output
 }
 
