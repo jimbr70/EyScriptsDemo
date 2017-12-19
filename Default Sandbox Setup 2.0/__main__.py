1a58c1d74ec81e64340aa3fa3748f70fb40777d5
Gil Galditsky <gil.g@qualisystems.com>
2017-12-19 11:24:51 +0200

    a

--- Default Sandbox Setup 2.0/__main__.py
+++ Default Sandbox Setup 2.0/__main__.py
@@ -11,10 +11,10 @@ def main():
 
 
 def azure_app_extention(sandbox, components):
-    # resource = sandbox.automation_api.GetResourceDetails("qs_cred_resource")
-    # password = next(iter(c for c in resource.ResourceAttributes if c == "password"), None)
-    # sandbox.automation_api.DecryptPassword(password)
-    # uname = next(iter(c for c in resource.ResourceAttributes if c == "uname"), None)
+    resource = sandbox.automation_api.GetResourceDetails("qs_cred_resource")
+    password = next(iter(c for c in resource.ResourceAttributes if c == "Password"), None)
+    sandbox.automation_api.DecryptPassword(password)
+    uname = next(iter(c for c in resource.ResourceAttributes if c == "User"), None)
     
 	#Configure Azure apps
     azure_from_market_apps = sandbox.components.get_apps_by_name_contains('azurefrommarket')
@@ -22,8 +22,8 @@ def azure_app_extention(sandbox, components):
                                                            message='Azure app from market found {0}'.format(len(azure_from_market_apps)))
     for app in azure_from_market_apps:
 		#inject user name and password to the repo
-        sandbox.apps_configuration.set_config_param(app=app,key='qspword',value="pword1")
-        sandbox.apps_configuration.set_config_param(app=app,key='qsuname',value="uname1")		
+        sandbox.apps_configuration.set_config_param(app=app,key='qspword',value=password)
+        sandbox.apps_configuration.set_config_param(app=app,key='qsuname',value=uname)		
         sandbox.automation_api.WriteMessageToReservationOutput(reservationId=sandbox.id,message='AzureApp configured')
 		
     sandbox.apps_configuration.apply_apps_configurations(azure_from_market_apps)
