80bd0dba4bbb4441771ee989730c91f04bde793c
Gil Galditsky <gil.g@qualisystems.com>
2017-12-19 14:33:16 +0200

    d

--- main.ps1
+++ main.ps1
@@ -14,9 +14,11 @@ For ($i=1; $i -le $script_count; $i++) {
     $script_name ="qsscript{0}_url" -f $i
     Write-host  ( $script_name)
     $script_url=(get-item env:$script_name).Value
-    Write-host  ( $script_url)
-    $output="c:\\{0}.txt" -f $script_name 
-
+    Write-host  ( $script_url)	
+	
+	$fileName = $url.Substring($url.LastIndexOf("/")+1)
+    $output="c:\\{0}.txt" -f $fileName
+	
     $msg= "`n downloading url : {0}"-f $script_url 
 	Add-Content $logspath $msg
 	
