fcae9022e023a18752e13b210188f41cc5157cc8
GilGald <GilGald@users.noreply.github.com>
2017-12-20 11:39:24 +0200

    Update downloadme1.ps1
    a

--- downloadme1.ps1
+++ downloadme1.ps1
@@ -1,3 +1,9 @@
-$logspath="c:\qs_scripts_logs_test_run.txt"
+param([Int32]$data)
+$logspath="c:\qs_scripts_logs_test_run{0}.txt" -f $data
 New-Item $logspath -type file -force
 Add-Content $logspath "`nstarts script download"
+Add-Content $logspath  $data
+
+#$logspath="c:\qs_scripts_logs_test_run.txt"
+#New-Item $logspath -type file -force
+#Add-Content $logspath "`nstarts script download"
