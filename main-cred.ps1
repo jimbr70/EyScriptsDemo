8b72d02830fd90f572cfae62d371f1772587ee30
jimbr70 <jim.b@qualisystems.com>
2018-01-07 23:57:31 -0800

    Update main-cred.ps1

--- main-cred.ps1
+++ main-cred.ps1
@@ -12,23 +12,27 @@ Add-Content $logspath "`nstarts script download"
 
 For ($i=1; $i -le $script_count; $i++) {
     $script_name ="qsscript{0}_url" -f $i
-   
     $script_url=(get-item env:$script_name).Value
+    Add-Content $logspath $script_url
+    
     $fileName = $script_url.Substring($script_url.LastIndexOf("/")+1)
     $output="c:\\{0}" -f $fileName
-	
+    Add-Content $logspath $output
+    
     $msg= "`n downloading url : {0}"-f $script_url 
     Add-Content $logspath $msg
 
     $user = $uname
     $pass = $pname
+    Add-Content $logspath $user
+    Add-Content $logspath $pass
     
     $securepassword = ConvertTo-SecureString $pass -AsPlainText -Force
     $credentials = New-Object System.Management.Automation.PSCredential($user, $securepassword)
     Invoke-WebRequest -Uri $script_url -Credential $credentials -OutFile $output
 }
 
-$msg= "`n got pass {0} and uname :{1} and execute command {2} "-f $pname,$uname,$execute_command
+$msg= "`n  execute command {2} "-f $pname,$uname,$execute_command
 Add-Content $logspath $msg
 
 $msg= "`n executing the command {0} "-f $execute_command
