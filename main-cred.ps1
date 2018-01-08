e49fbdf64e56a7fa3bf2e41c602e463912121aa2
jimbr70 <jim.b@qualisystems.com>
2018-01-08 01:12:20 -0800

    Update main-cred.ps1

--- main-cred.ps1
+++ main-cred.ps1
@@ -10,7 +10,9 @@ $execute_command=(get-item env:qsexecute_command).Value
 New-Item $logspath -type file -force
 Add-Content $logspath "`nstarts script download"
 
+
 For ($i=1; $i -le $script_count; $i++) {
+    Try {
     $script_name ="qsscript{0}_url" -f $i
     $script_url=(get-item env:$script_name).Value
     Add-Content $logspath $script_url
@@ -30,6 +32,14 @@ For ($i=1; $i -le $script_count; $i++) {
     $securepassword = ConvertTo-SecureString $pass -AsPlainText -Force
     $credentials = New-Object System.Management.Automation.PSCredential($user, $securepassword)
     Invoke-WebRequest -Uri $script_url -Credential $credentials -OutFile $output
+    }
+    Catch {
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+    Add-Content $logspath $ErrorMessage
+    Add-Content $logspath $FailedItem
+    }
+    Add-Content $logspath "-----------------------------------------------"
 }
 
 $msg= "`n  execute command {2} "-f $pname,$uname,$execute_command
