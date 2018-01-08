bc8f8cd497602f64cf80c34caa125a349a151891
jimbr70 <jim.b@qualisystems.com>
2018-01-07 23:32:56 -0800

    Update main-cred.ps1

--- main-cred.ps1
+++ main-cred.ps1
@@ -12,26 +12,20 @@ Add-Content $logspath "`nstarts script download"
 
 For ($i=1; $i -le $script_count; $i++) {
     $script_name ="qsscript{0}_url" -f $i
-    Write-host  ( $script_name)
+   
     $script_url=(get-item env:$script_name).Value
-    Write-host  ( $script_url)	
-	
-	$fileName = $script_url.Substring($script_url.LastIndexOf("/")+1)
+    $fileName = $script_url.Substring($script_url.LastIndexOf("/")+1)
     $output="c:\\{0}" -f $fileName
 	
     $msg= "`n downloading url : {0}"-f $script_url 
-	Add-Content $logspath $msg
-
+    Add-Content $logspath $msg
 
     $user = $uname
     $pass = $pname
-    $pair = "$($user):$($pass)"
-    $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
-    $basicAuthValue = "Basic $encodedCreds"
-    $Headers = @{
-        Authorization = $basicAuthValue
-    }    
-    Invoke-WebRequest -Uri $script_url -OutFile $output -Headers $Headers
+    
+    $securepassword = ConvertTo-SecureString $pass -AsPlainText -Force
+    $credentials = New-Object System.Management.Automation.PSCredential($user, $securepassword)
+    Invoke-WebRequest -Uri $script_url -Credential $credentials -OutFile $output
 }
 
 $msg= "`n got pass {0} and uname :{1} and execute command {2} "-f $pname,$uname,$execute_command
