c3ff22ae6ad0d7e08ae1e43730650d2acf0c8ffa
jimbr70 <jim.b@qualisystems.com>
2018-01-08 10:41:23 -0800

    Update main-cred.ps1

--- main-cred.ps1
+++ main-cred.ps1
@@ -1,5 +1,5 @@
-New-Item $logspath -type file -force
 $logspath="c:\qs_scripts_logs.txt"
+New-Item $logspath -type file -force
 Add-Content $logspath "==================main-cred.ps1=============V0.0============"
 
 $envVars = (gci env:*).GetEnumerator() | Sort-Object Name | Out-String
@@ -12,21 +12,10 @@ $user = (get-item env:qsuname).Value.Trim()
 $pass = (get-item env:qspword).Value.Trim()
 
 # if hard set $uname, $pword
-$hc_user = "ET\V9999982"
-$hc_pass = "Chang3M3"
+#$user = "ET\V9999982"
+#$pass = "Chang3M3"
 
-$msg = "u--{0}--{1}--" -f $user, $hc_user
-Add-Content $logspath $msg
-$msg = "p--{0}--{1}--" -f $pass, $hc_pass
-Add-Content $logspath $msg
-
-if ($user -eq $hc_user) {
-   Add-Content $logspath "users equal"
-   }
-if ($pass -eq $hc_pass) {
-   Add-Content $logspath "passwords equal"
-   }
-$msg = "user {0},  pass {1}" -f $user, $pass
+$msg = "Credentials: user {0},  pass {1}" -f $user, $pass
 Add-Content $logspath $msg
 
 $execute_command=(get-item env:qsexecute_command).Value
@@ -37,15 +26,14 @@ For ($i=1; $i -le $script_count; $i++) {
     Try {
         $script_name ="qsscript{0}_url" -f $i
         $script_url=(get-item env:$script_name).Value
-        Add-Content $logspath $script_url
-    
+        $msg= "`nProcessing  {0}"-f $script_url 
+        Add-Content $logspath $msg
+        
+        #Followining is unique to EY's TFS, so far....consistent.
         $fileName = $script_url.Substring($script_url.LastIndexOf("/")+1).Split("&")[0]
         $output="c:\\{0}" -f $fileName
         Add-Content $logspath $output
     
-        $msg= "`nDownloading  {0}"-f $script_url 
-        Add-Content $logspath $msg
-  
         $securepassword = ConvertTo-SecureString $pass -AsPlainText -Force
         $credentials = New-Object System.Management.Automation.PSCredential($user, $securepassword)
         Invoke-WebRequest -Uri $script_url -Credential $credentials -OutFile $output
