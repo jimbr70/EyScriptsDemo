c222a6430a9e36100a898984b2748d7e11776221
jimbr70 <jim.b@qualisystems.com>
2018-01-09 09:30:45 -0800

    Update __main__.py
    Added insertion of reservationID, changed to  check for Azure in name, not a longer name

--- Default Sandbox Setup 2.0/__main__.py
+++ Default Sandbox Setup 2.0/__main__.py
@@ -9,32 +9,32 @@ def main():
     sandbox.execute_setup()
 
 
-
 def azure_app_extention(sandbox, components):
-    resource = sandbox.automation_api.GetResourceDetails("qs_cred_resource")
+    resource = sandbox.automation_api.GetResourceDetails("Globals_Resource")
 
     sandbox.automation_api.WriteMessageToReservationOutput(reservationId=sandbox.id,
-                                                           message='Reading data from qs resource')
+                                                           message='Reading data from globals resource')
 
-    password = next((c.Value for c in resource.ResourceAttributes if (c.Name == "Password")), None)
-    decrypt_password = sandbox.automation_api.DecryptPassword(password).Value
-    uname = next((c.Value for c in resource.ResourceAttributes if (c.Name == "User")), None)
+    tfs_password = next((c.Value for c in resource.ResourceAttributes if (c.Name == "TFS_Password")), None)
+    decrypt_password = sandbox.automation_api.DecryptPassword(tfs_password).Value
+    tfs_uname = next((c.Value for c in resource.ResourceAttributes if (c.Name == "TFS_User")), None)
 
     sandbox.automation_api.WriteMessageToReservationOutput(reservationId=sandbox.id,
-                                                           message='Uname {0} and powrd {1} found '.format(uname,decrypt_password))
+                                                           message='Uname {0} and pword {1} found '.format(uname,decrypt_password))
 
     #Configure Azure apps
-    azure_from_market_apps = sandbox.components.get_apps_by_name_contains('AzureFromMarket')
+    azure_apps = sandbox.components.get_apps_by_name_contains('Azure')
     sandbox.automation_api.WriteMessageToReservationOutput(reservationId=sandbox.id,
-                                                           message='AzureFromMarket found {0}'.format(len(azure_from_market_apps)))
+                                                           message='{0} Azure apps found '.format(len(azure_apps)))
 
-    for app in azure_from_market_apps:
-        #inject user name and password to the repo
+    for app in azure_apps:
+        #inject rsvnID, user name and password to the repo
+        sandbox.apps_configuration.set_config_param(app=app,key='reservation_id',value=sandbox.id)
         sandbox.apps_configuration.set_config_param(app=app,key='qspword',value=decrypt_password)
         sandbox.apps_configuration.set_config_param(app=app,key='qsuname',value=uname)
         sandbox.automation_api.WriteMessageToReservationOutput(reservationId=sandbox.id,message='AzureFromMarket')
 
-    sandbox.apps_configuration.apply_apps_configurations(azure_from_market_apps)
+    sandbox.apps_configuration.apply_apps_configurations(azure_apps)
     sandbox.automation_api.WriteMessageToReservationOutput(reservationId=sandbox.id,message='Finished configuring Azure apps')
 
 main()
