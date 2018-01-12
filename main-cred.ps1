71b08fed6b4776231fb604d58f93c495004e62fc
jimbr70 <jim.b@qualisystems.com>
2018-01-12 08:15:58 -0800

    Update main-cred.ps1

--- main-cred.ps1
+++ main-cred.ps1
@@ -81,8 +81,8 @@ Catch {
 #ftp phase code goes here once we have download link for psftp.
 
 #Create meta-data file for EY scripts to leverage
-$meta-data = "rsvn-meta-data.txt"
-$junk = New-Item $meta-data -type file -force
+$metadata = "rsvn-meta-data.txt"
+$junk = New-Item $metadata -type file -force
 Add-Content $metadata 'rsvnId:{0}\n'.format(sandbox.id)
 Add-Content $metadata 'ftp_server:{0}\n'.format(ftp_server)
 Add-Content $metadata 'ftp_user:{0}\n'.format(ftp_uname)
