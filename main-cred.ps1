3ab5c6037e19374f39967eba14f9a042a8761186
jimbr70 <jim.b@qualisystems.com>
2018-01-12 11:39:11 -0800

    Update main-cred.ps1

--- main-cred.ps1
+++ main-cred.ps1
@@ -94,6 +94,7 @@ Try {
     Add-Content $ftp_commands "quit"
     $listing = C:\\psftp -l $ftpuser -pw $ftppass -b "C:\\ftp_commands.cmd" $ftpsrvr
     Remove-Item $ftp_commands
+    Add-Content $logspath "first done"
     
     $junk = New-Item $ftp_commands -type file -force
     Add-Content $ftp_commands "cd Logs"
