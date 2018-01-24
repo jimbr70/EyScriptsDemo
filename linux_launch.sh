fa6779841cb1667ccbe749ee4083ad42efde02d0
jimbr70 <jim.b@qualisystems.com>
2018-01-24 10:20:29 -0800

    Update linux_launch.sh

--- linux_launch.sh
+++ linux_launch.sh
@@ -5,7 +5,7 @@ cd /tmp/csd
 
 curl https://raw.githubusercontent.com/jimbr70/EyScriptsDemo/master/linux_deploy.py -o linux_deploy.py
 
-#curl --ntlm --user 'L.CMFCS.01:JjFjtX!#E+D8w63Q' 'http://10.254.34.24:8080/tfs/DefaultCollection/d3782bb2-7bbf-48fd-8f3a-0b7e24507bac/_api/_versioncontrol/itemContent?repositoryId=78d6293c-71cd-4c07-8e3e-6f85e30e6144&path=/sccm/Deploy-SCCMClient.ps1&version=GBmaster&contentOnly=false&__v=5' -o bla.py
+#curl --ntlm --user '$qsuname:$qspword' 'http://10.254.34.24:8080/tfs/DefaultCollection/d3782bb2-7bbf-48fd-8f3a-0b7e24507bac/_api/_versioncontrol/itemContent?repositoryId=78d6293c-71cd-4c07-8e3e-6f85e30e6144&path=/sccm/Deploy-SCCMClient.ps1&version=GBmaster&contentOnly=false&__v=5' -o bla.py
 
 python ./linux_deploy.py
 
